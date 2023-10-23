923a37aa200181ac9015f83c9f2c891111de5c26
Ivan PM <cdmx2125@amerike.edu.mx>
2023-10-22 19:17:47 -0600

    I CANT STOP SINGING THIS BLOODY TOON TOON TOON

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Ivan Paniagua Maldonado
 
 Práctica DOMVERSO
 
-https://github.com/DomnhallIvan/EVENTOS-SAYWHAAAT.github.io
+https://github.com/DomnhallIvan/TickTock.github.io
 
-https://domnhallivan.github.io/EVENTOS-SAYWHAAAT.github.io/
+https://domnhallivan.github.io/TickTock.github.io/
 
--- cosas.js
+++ cosas.js
@@ -1,37 +1,5 @@
 console.log(document);
 
-document.write("<h1>Ivan Paniagua Maldonado</h1>");
-
-
-let $foto=document.createElement("img");
-$foto.src = "https://cdn.discordapp.com/attachments/845383920458924034/1011653416466006067/IMG_20220823_100825.jpg?ex=6537de2a&is=6525692a&hm=ac191cc738377cfcdc1c2d67918ebdb43f165e0feb6c1c6ae8e7c5848bbad5a7&";
-$foto.style.maxWidth="30%";
-
-document.body.appendChild($foto);
-
-document.write("<p>Nací en la Ciudad de México el 5 de Febrero de 2003,siempre alguien muy sociable pero reservado y que nunca he dejado de amar los videojuegos que al día de hoy seguiré estudiando programación hasta un día ser un Senior Developer de Videojuegos y un gran amigo.")
-
-
-const hobbies = ["Jugar Videojuegos", "Sacar a mi perro y jugar con él", "Ver series y vídeos", "Leer","Caminar"],
-  $ol = document.createElement("ol");
-
-document.write("<h3>Mis pasatiempos</h3>");
-document.body.appendChild($ol);
-
-hobbies.forEach((el) => {
-  const $li = document.createElement("li");
-  $li.textContent = el;
-  $ol.appendChild($li);
-});
-
-const contactos = ["Numero Tel: 5580335774", "Correo: ipaniaguamaldonado@gmail.com"],
-  $ul2 = document.createElement("ul");
-
-document.write("<h3>Contactos</h3>");
-document.body.appendChild($ul2);
-$ul2.innerHTML = "";
-contactos.forEach((el) => ($ul2.innerHTML += `<li>${el}</li>`));
-
 
 $btnTheme = document.querySelector("#btn-theme");
 
